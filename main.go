48ba9bfc152add9a31fd0fd934554512123ee329
mpl <mathieu.lonjaret@gmail.com>
2013-03-31 17:31:35 +0200

    lexicographic sort.
    simply copied code from the http package and hacked it to add the
    sorting. probably not the most elegant.

--- main.go
+++ main.go
@@ -17,8 +17,9 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"path"
+	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
 )
@@ -110,9 +111,109 @@ func sendUnauthorized(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "<html><body><h1>Unauthorized</h1>")
 }
 
-// because getting a 404 when trying to use http.FileServer. beats me.
+type sortedFiles []os.FileInfo
+
+func (s sortedFiles) Len() int { return len(s) }
+
+func (s sortedFiles) Less(i, j int) bool {
+	return strings.ToLower(s[i].Name()) < strings.ToLower(s[j].Name())
+}
+
+func (s sortedFiles) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func sortedDirList(w http.ResponseWriter, f http.File) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<pre>\n")
+	var sdirs sortedFiles
+	for {
+		dirs, err := f.Readdir(100)
+		if err != nil || len(dirs) == 0 {
+			break
+		}
+		sdirs = append(sdirs, dirs...)
+	}
+	sort.Sort(sdirs)
+	for _, d := range sdirs {
+		name := d.Name()
+		if d.IsDir() {
+			name += "/"
+		}
+		// TODO htmlescape
+		fmt.Fprintf(w, "<a href=\"%s\">%s</a>\n", name, name)
+	}
+	fmt.Fprintf(w, "</pre>\n")
+}
+
+// modtime is the modification time of the resource to be served, or IsZero().
+// return value is whether this request is now complete.
+func checkLastModified(w http.ResponseWriter, r *http.Request, modtime time.Time) bool {
+	if modtime.IsZero() {
+		return false
+	}
+
+	// The Date-Modified header truncates sub-second precision, so
+	// use mtime < t+1s instead of mtime <= t to check for unmodified.
+	if t, err := time.Parse(http.TimeFormat, r.Header.Get("If-Modified-Since")); err == nil && modtime.Before(t.Add(1*time.Second)) {
+		w.WriteHeader(http.StatusNotModified)
+		return true
+	}
+	w.Header().Set("Last-Modified", modtime.UTC().Format(http.TimeFormat))
+	return false
+}
+
+// name is '/'-separated, not filepath.Separator.
+func serveFile(w http.ResponseWriter, r *http.Request, fs http.FileSystem, name string) {
+	const indexPage = "/index.html"
+
+	f, err := fs.Open(name)
+	if err != nil {
+		// TODO expose actual error?
+		http.NotFound(w, r)
+		return
+	}
+	defer f.Close()
+
+	d, err1 := f.Stat()
+	if err1 != nil {
+		// TODO expose actual error?
+		http.NotFound(w, r)
+		return
+	}
+
+	// use contents of index.html for directory, if present
+	if d.IsDir() {
+		index := name + indexPage
+		ff, err := fs.Open(index)
+		if err == nil {
+			defer ff.Close()
+			dd, err := ff.Stat()
+			if err == nil {
+				name = index
+				d = dd
+				f = ff
+			}
+		}
+	}
+
+	// Still a directory? (we didn't find an index.html file)
+	if d.IsDir() {
+		if checkLastModified(w, r, d.ModTime()) {
+			return
+		}
+		sortedDirList(w, f)
+		return
+	}
+
+	// serverContent will check modification time
+	http.ServeContent(w, r, d.Name(), d.ModTime(), f)
+}
+
 func myFileServer(w http.ResponseWriter, r *http.Request, url string) {
-	http.ServeFile(w, r, path.Join(rootdir, url))
+//	http.ServeFile(w, r, path.Join(rootdir, url))
+	dir, file := filepath.Split(filepath.Join(rootdir, url))
+	serveFile(w, r, http.Dir(dir), file)
 }
 
 func uploadHandler(rw http.ResponseWriter, req *http.Request, url string) {
@@ -141,7 +242,7 @@ func uploadHandler(rw http.ResponseWriter, req *http.Request, url string) {
 			http.Error(rw, "copying: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
-		f, err := os.Create(path.Join(rootdir, fileName))
+		f, err := os.Create(filepath.Join(rootdir, fileName))
 		if err != nil {
 			http.Error(rw, "opening file: "+err.Error(), http.StatusInternalServerError)
 			return
@@ -154,7 +255,7 @@ func uploadHandler(rw http.ResponseWriter, req *http.Request, url string) {
 		}
 		println(fileName + " uploaded")
 	}
-	http.ServeFile(rw, req, path.Join(rootdir, uploadform))
+	http.ServeFile(rw, req, filepath.Join(rootdir, uploadform))
 }
 
 func createUploadForm() {
@@ -175,7 +276,7 @@ func createUploadForm() {
 </body>
 </html>
 `
-	f, err := os.Create(path.Join(rootdir, uploadform))
+	f, err := os.Create(filepath.Join(rootdir, uploadform))
 	if err != nil {
 		println("err creating uploadform")
 		os.Exit(2)
